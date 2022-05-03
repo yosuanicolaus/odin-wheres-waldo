864a8e4d0a1b8e699dbff7f060a60e028166e5c9
Yosua Nicolaus <cryodawn257@gmail.com>
2022-05-03 10:55:06 +0700

    add and test waldo images

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+const waldoSrc = "./assets/wheres-waldo-1.jpg";
+
 function App() {
   return (
     <div>
       <div className="display-1">Hello world</div>
+      <img
+        src={waldoSrc}
+        alt=""
+        className="img-fluid"
+        onClick={(e) => {
+          alert("you clicked " + e.clientX + ", " + e.clientY);
+        }}
+      />
     </div>
   );
 }
